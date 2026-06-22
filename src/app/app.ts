e65411c0ee4a99dd9f24eea401b814c6da39cbaa
megha7-lab <24f2003594@ds.study.iitm.ac.in>
2026-06-22 14:49:31 +0530

    feat: complete parent directory structure upload including login components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('muster_management');
+}
